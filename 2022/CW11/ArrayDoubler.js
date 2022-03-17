22d9247c034f7cec5ce86bcfb6b0e2ec612751d5
James <8533782+ShockleyJE@users.noreply.github.com>
2022-03-17 17:29:21 -0400

    Create ArrayDoubler.js
    Adding exercise ArrayDoubler

---
+++ 2022/CW11/ArrayDoubler.js
@@ -0,0 +1,14 @@
+// Attempt 1: Using array.map
+
+let double = (x) => x * 2;
+
+function maps(x){
+  let dbldarray = x.map(double);
+  return dbldarray
+}
+
+// OR, one-liner
+
+function maps(x){
+    return dbldarray = x.map((x) => x * 2);
+}
