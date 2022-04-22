40855ba0ebff74ea7baf0274f702d337bf42de21
shockleyje <8533782+ShockleyJE@users.noreply.github.com>
2022-04-22 17:43:31 -0400

    Update HashtagGenerator with other solution

--- 2022/CW16/HashtagGenerator.js
+++ 2022/CW16/HashtagGenerator.js
@@ -23,3 +23,16 @@ function generateHashtag (str) {
       }
     }
   }
+
+  //Solution 1, much cleaner than mine. However, it doesn't handle the space string case.
+
+  function generateHashtag (str) {
+    return str.length > 140 || str === '' ? false :
+      '#' + str.split(' ').map(capitalize).join('');
+  }
+  
+  function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  // I didn't really like the other solutions. They were less readable & clear
