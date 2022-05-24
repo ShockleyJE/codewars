e44d3c191f29eabe0af4068c8786bd4db4a73fee
shockleyje <8533782+ShockleyJE@users.noreply.github.com>
2022-05-24 18:24:41 -0400

    Quick assignment before appointment

---
+++ 2022/CW21/HelloWolrd.js
@@ -0,0 +1,8 @@
+// Kyu 8
+//https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8/solutions/javascript
+// I am late for an appointment but will not miss a day
+
+function helloWorld() {
+  let str = "Hello World!";
+  console.log(str);
+}
