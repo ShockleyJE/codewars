a553e292fe12b0fd80ff956fa22a85e2e03090ca
shockleyje <8533782+ShockleyJE@users.noreply.github.com>
2022-07-05 20:02:32 -0400

    Backspaces in Strings

--- 2022/CW27/BackspacesInString.js
+++ 2022/CW27/BackspacesInString.js
@@ -14,3 +14,11 @@ function cleanString(s) {
 
 console.log(cleanString("abc#d##c"));
 console.log(cleanString("abc####d##c#"));
+
+// This is a pretty clean implementation too
+
+function cleanString(s) {
+  let result = [];
+  [...s].map((char) => (char === "#" ? result.pop() : result.push(char)));
+  return result.join("");
+}
