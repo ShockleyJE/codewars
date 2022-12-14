e052a64e7a1ef9c4ff933eb173ec517b03adfa68
shockleyje <8533782+ShockleyJE@users.noreply.github.com>
2022-12-13 20:39:13 -0500

    easy problem

---
+++ 2022/CW50/FriendOrFoe.js
@@ -0,0 +1,6 @@
+//I AM VERY BUSY THIS WEEK SO I CHOOSE AN EASY PROBLEM BECAUSE I NEVER SKIP A DAY
+
+//https://www.codewars.com/kata/55b42574ff091733d900002f/train/javascript
+function friend(friends) {
+  return friends.filter((x) => x.length == 4);
+}
